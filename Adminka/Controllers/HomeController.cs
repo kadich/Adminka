51c6735296db9f711d2518c0d0e689dd4ae6e76e
kadich <35394916+kadich@users.noreply.github.com>
2018-09-22 11:20:01 +0300

    underway

---
+++ Adminka/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Adminka.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Adminka.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ProductCreation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProductCreation(ProductParameters productParameters)
+        {
+            return View();
+        }
+    }
+}
---
+++ Adminka/Models/ProductParameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Adminka.Models
+{
+    public class ProductParameters
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public bool InStock { get; set; }
+    }
+}
