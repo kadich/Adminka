7f2fd038a208edfe86fa22b800a1c04cdfa6a84c
kadich <35394916+kadich@users.noreply.github.com>
2018-09-23 17:57:51 +0300

    added the ability to edit products

--- Adminka/Controllers/HomeController.cs
+++ Adminka/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Adminka.Controllers
     {
         AdminkaContext db = new AdminkaContext();
 
-        public ActionResult Index()
+        public ActionResult Index() //метод для отображения главной страницы
         {
             List<ProductParameters> products;
             
@@ -36,33 +36,49 @@ namespace Adminka.Controllers
         }
 
         [HttpGet]
-        public ActionResult ProductCreation()
+        public ActionResult ProductCreation() //метод для отображения страницы создания нового товара
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult ProductCreation(ProductEntity productParameters)
+        public ActionResult ProductCreation(ProductEntity product) //метод для записи товара в БД
         {
             if (!ModelState.IsValid)
             {
-                return View(productParameters);
+                return View(product);
             }
 
-            db.Products.Add(productParameters);
+            db.Products.Add(product);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
 
         [HttpPost]
-        public ActionResult Delete(ProductEntity productParameters)
+        public ActionResult Delete(ProductEntity product) //метод для удаления товара
         {
-            var b = db.Products.Find(productParameters.Id);
+            var b = db.Products.Find(product.Id);
             if (b != null)
             {
                 db.Products.Remove(b);
                 db.SaveChanges();
             }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id) //метод для отображения страницы редактированяи товара
+        {
+            var product = db.Products.Find(id);
+            
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProductEntity product) //метод для внесения изменений в БД
+        {
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
             return RedirectToAction("Index");
         }
     }
